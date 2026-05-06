042656f6f4fe8d4790226b11a833fb5e486d1748
Marpo <son127024@gmail.com>
2026-05-05 23:36:36 -0700

    파이 브라우저용 정적 파일(out 폴더)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  output: 'export',      // 🚩 이 줄이 들어가야 빌드 시 'out' 폴더가 생성됩니다!
   images: {
-    unoptimized: true,
+    unoptimized: true,   // 정적 익스포트 시 익스포트 에러를 방지하기 위해 필수입니다.
   },
-
 };
 
-module.exports = nextConfig; // export default 대신 이 형식을 씁니다.
+module.exports = nextConfig;
